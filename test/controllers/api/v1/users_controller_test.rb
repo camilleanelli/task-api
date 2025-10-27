d3ea0444bb56c1207754ae6a06edf381d2f0ec36
Camille Anelli <camilleanelli32@gmail.com>
2025-10-27 09:42:08 +0100

    test user creation

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -5,21 +5,26 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     @user1 = users(:one)
     @user2 = users(:two)
   end
-  test "should return users info" do
+  test "should return users emails and id" do
     get api_v1_users_url
     assert_response :success
     assert_includes @response.body, @user1.email
-    assert_includes @response.body, @user1.name
-    assert_includes @response.body, @user1.last_name
+    assert_includes @response.body, @user1.id.to_s
   end
 
-  # test "should create user with valid params" do
-  #   post api_v1_users_create_url
-  #   assert_response :success
-  # end
+  test "should create user with valid params" do
+    assert_difference("User.count") do
+      post api_v1_users_url, params: { user: { name: "New", last_name: "User", email: "new_user@example.com" } }
+    end
+    assert_response :created
+    json_response = JSON.parse(@response.body)
+    assert_includes json_response.values, "New"
+    assert_includes json_response.values, "User"
+    assert_includes json_response.values, "new_user@example.com"
+  end
 
-  # test "should return error with invalid params" do
-  #   post api_v1_users_create_url
-  #   assert_response :success
-  # end
+  test "should return error with invalid params" do
+    post api_v1_users_url, params: { user: { name: "", last_name: "User", email: "invalid_email" } }
+    assert_response :unprocessable_entity
+  end
 end
