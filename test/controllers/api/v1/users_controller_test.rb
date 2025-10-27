97b335f22165213070e7fd5ae53bcee721956071
Camille Anelli <camilleanelli32@gmail.com>
2025-10-27 09:23:37 +0100

    test index users

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,23 +1,25 @@
 require "test_helper"
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get api_v1_users_index_url
+  setup do
+    @user1 = users(:one)
+    @user2 = users(:two)
+  end
+  test "should return users info" do
+    get api_v1_users_url
     assert_response :success
+    assert_includes @response.body, @user1.email
+    assert_includes @response.body, @user1.name
+    assert_includes @response.body, @user1.last_name
   end
 
-  test "should get create" do
-    get api_v1_users_create_url
-    assert_response :success
-  end
+  # test "should create user with valid params" do
+  #   post api_v1_users_create_url
+  #   assert_response :success
+  # end
 
-  test "should get update" do
-    get api_v1_users_update_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get api_v1_users_destroy_url
-    assert_response :success
-  end
+  # test "should return error with invalid params" do
+  #   post api_v1_users_create_url
+  #   assert_response :success
+  # end
 end
