1fbb413135950621efe15a94aef97c115ba69051
Camille Anelli <camilleanelli32@gmail.com>
2025-10-27 08:55:05 +0100

    add model user

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
