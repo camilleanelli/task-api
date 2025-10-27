0eac816557c75c7141921edc9deec5254304611f
Camille Anelli <camilleanelli32@gmail.com>
2025-10-27 09:42:16 +0100

    test user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,11 +5,21 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :last_name, :email)
   end
 end
