59cdab51a14ebd91bbe073376e4ca2e9d8b111d2
Camille Anelli <camilleanelli32@gmail.com>
2025-11-13 15:57:07 +0100

    test

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -12,16 +12,14 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_includes @response.body, @user1.id.to_s
   end
 
-  # test "should create user with valid params" do
-  #   assert_difference("User.count") do
-  #     post api_v1_users_url, params: { user: { name: "New", last_name: "User", email: "new_user@example.com" } }
-  #   end
-  #   assert_response :created
-  #   json_response = JSON.parse(@response.body)
-  #   assert_includes json_response.values, "New"
-  #   assert_includes json_response.values, "User"
-  #   assert_includes json_response.values, "new_user@example.com"
-  # end
+  test "should create user with valid params" do
+    assert_difference("User.count") do
+      post api_v1_users_url, params: { user: { email: "new_user@example.com" } }
+    end
+    assert_response :created
+    json_response = JSON.parse(@response.body)
+    assert_includes json_response.values, "new_user@example.com"
+  end
 
   # test "should return error with invalid params" do
   #   post api_v1_users_url, params: { user: { name: "", last_name: "User", email: "invalid_email" } }
